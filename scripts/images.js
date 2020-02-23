20e01632f1f85ea79a25185bae365df7928a3f82
Samantha Nell <samanthanell@Samanthas-MacBook-Pro.local>
2020-02-23 16:33:36 +0200

    couples guidance all views completed

--- scripts/images.js
+++ scripts/images.js
@@ -1,13 +1,4 @@
 
-// about page
-const aboutImage = document.querySelector('.aboutImage');
-const aboutDesktop = "assets/images/PB_couplesSession_Desktop.jpg";
-const aboutMobile = "assets/images/PB_couplesSession.jpg";
-// therapy page
-const therapyImage = document.querySelector('.therapyImage');
-const therapyDesktop = "assets/images/PBrowde_intimateCouplesSession_Desktop.jpg";
-const therapyMobile = "assets/images/PBrowde_intimateCouplesSession.jpg";
-
 const imageResize = (image, desktopSrc, mobileSrc) => {
     if(screen.width >=1200){
         image.src = desktopSrc;
